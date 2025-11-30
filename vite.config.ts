4f15c9997fe589e23c6c75547bbd01fe42a8c80e
ngmtine <ngmfries@gmail.com>
2025-11-30 15:37:41 +0900

    feat: tailwind, remark, rehype, front-matter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import honox from "honox/vite";
 import { defineConfig } from "vite";
 import ssrPlugin from "vite-ssr-components/plugin";
 
-export default defineConfig({
-    plugins: [cloudflare(), ssrPlugin()],
+const config = defineConfig({
+    plugins: [
+        cloudflare(),
+        ssrPlugin(),
+        tailwindcss(),
+        honox({
+            devServer: { adapter },
+            client: { input: ["/src/client.ts", "/src/style.css"] },
+        }),
+    ],
 });
+
+export default config;
