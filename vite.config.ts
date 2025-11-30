42111ea6b5ef3910e1740b2402f9349384164d67
ngmtine <ngmfries@gmail.com>
2025-11-30 18:46:08 +0900

    mv: old

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import devServer from "@hono/vite-dev-server";
+import build from "@hono/vite-build/cloudflare-workers";
+import adapter from "@hono/vite-dev-server/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
+import honox from "honox/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
-        devServer({
-            entry: "src/index.tsx",
+        honox({
+            devServer: { adapter },
+            client: { input: ["/app/client.ts", "/app/style.css"] },
         }),
+        tailwindcss(),
+        build(),
     ],
 });
