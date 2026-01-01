a57aad3331c492ed13853d7028fdd7b176d9114c
ngmtine <ngmfries@gmail.com>
2026-01-01 20:29:35 +0900

    feat: スタイル調整

--- app/components/pages/GenrePage.tsx
+++ app/components/pages/GenrePage.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 export const GenrePage = ({ genre, posts }: Props) => (
-    <div class="rounded-xl border border-gray-300 bg-slate-200 p-2 dark:border-gray-700 dark:bg-gray-850">
+    <div class="rounded-xl border border-gray-400 bg-slate-200 p-2 dark:border-gray-700 dark:bg-gray-850">
         <h1 class="p-2 font-bold text-4xl">{genre}</h1>
         <div>
             {posts.map((post) => (
