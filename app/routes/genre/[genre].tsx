4539629ee9c2d9bdbf646428c5cfe38e28c7b16c
ngmtine <ngmfries@gmail.com>
2026-01-03 18:34:07 +0900

    fix: ssg動いて無くて個別ページが404だったの修正

--- app/routes/genre/[genre].tsx
+++ app/routes/genre/[genre].tsx
@@ -4,33 +4,34 @@ import { createRoute } from "honox/factory";
 import { GenrePage } from "../../components/pages/GenrePage";
 import { getAllPosts } from "../../lib/posts";
 
-// SSG: Generate static files for each genre
-export const ssg = ssgParams(async () => {
-    const posts = await getAllPosts();
-    const genreList = [
-        ...new Set(
-            posts
-                .map((post) => post.frontmatter.genre) //
-                .filter((genre): genre is string => Boolean(genre)),
-        ),
-    ];
-    return genreList.map((genre) => ({ genre: genre.toLowerCase() }));
-});
+export default createRoute(
+    // SSG: Generate static files for each genre
+    ssgParams(async () => {
+        const posts = await getAllPosts();
+        const genreList = [
+            ...new Set(
+                posts
+                    .map((post) => post.frontmatter.genre) //
+                    .filter((genre): genre is string => Boolean(genre)),
+            ),
+        ];
+        return genreList.map((genre) => ({ genre: genre.toLowerCase() }));
+    }),
+    async (c) => {
+        const genre = c.req.param("genre");
+        if (!genre) return c.notFound();
 
-export default createRoute(async (c) => {
-    const genre = c.req.param("genre");
-    if (!genre) return c.notFound();
+        const posts = await getAllPosts();
+        const filteredPosts = posts.filter(
+            (post) => post.frontmatter.genre?.toLowerCase() === genre.toLowerCase(), //
+        );
+        if (filteredPosts.length === 0) return c.notFound();
 
-    const posts = await getAllPosts();
-    const filteredPosts = posts.filter(
-        (post) => post.frontmatter.genre?.toLowerCase() === genre.toLowerCase(), //
-    );
-    if (filteredPosts.length === 0) return c.notFound();
-
-    return c.render(
-        <GenrePage
-            genre={genre} //
-            posts={filteredPosts}
-        />,
-    );
-});
+        return c.render(
+            <GenrePage
+                genre={genre} //
+                posts={filteredPosts}
+            />,
+        );
+    },
+);
