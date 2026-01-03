43e1a2ff56e385945816daa5bd793556e0df0bb1
ngmtine <ngmfries@gmail.com>
2026-01-03 23:30:43 +0900

    wip: サイドバーの透け

--- app/components/sidebarWrapper.tsx
+++ app/components/sidebarWrapper.tsx
@@ -17,7 +17,7 @@ export const SidebarWrapper = ({ posts, genreList }: Props) => (
         {/* サイドバー: モバイルはドロワー、デスクトップは常時表示 */}
         <aside
             id="mobile-sidebar"
-            class="fixed top-0 left-0 z-50 h-full w-[85vw] max-w-85 -translate-x-full overflow-y-auto p-4 pt-16 transition-transform duration-300 md:sticky md:z-auto md:h-screen md:w-70 md:min-w-70 md:max-w-none md:translate-x-0 md:overflow-y-hidden md:p-0 md:pt-20 md:pl-4"
+            class="fixed top-0 left-0 z-50 h-full w-[85vw] max-w-85 -translate-x-full overflow-y-auto bg-white/80 p-4 pt-16 transition-transform duration-300 md:sticky md:z-auto md:h-screen md:w-70 md:min-w-70 md:max-w-none md:translate-x-0 md:overflow-y-hidden md:p-0 md:pt-20 md:pl-4 dark:bg-black/80"
         >
             <Sidebar
                 posts={posts} //
